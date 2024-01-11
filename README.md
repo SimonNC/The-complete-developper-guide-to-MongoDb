fea6d5d84d6c1416a52c44810eb4f6750476553c
SimonNC <simon.jorite@gmail.com>
2024-01-11 11:07:29 +1100

    Basic crud operations

---
+++ README.md
@@ -0,0 +1,1 @@
+# The-complete-developper-guide-to-MongoDb
---
+++ test/test_helper.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+before(done => {
+  mongoose.connect("mongodb://127.0.0.1:27017/users_test");
+  mongoose.connection
+    .once("open", () => {
+      done();
+    })
+    .on("error", error => {
+      console.warn("Warning ", error);
+    });
+});
+
+beforeEach(done => {
+  mongoose.connection.collections.users.drop(() => {
+    //ready to run the next task
+    done();
+  });
+});
